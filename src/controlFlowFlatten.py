3482979bd08dcb8fa08c12e99b43c6f702d3b441
YuSheng <goudunz1@outlook.com>
2024-11-25 13:17:04 +0800

    control flow flatten: fixed bugs on CFG creation

--- src/controlFlowFlatten.py
+++ src/controlFlowFlatten.py
@@ -75,6 +75,31 @@ class CFG:
         self.init_state = self.gen_state()
         self.end_state = self.gen_state()
 
+    def add_bb(
+        self,
+        state: int,
+        next_state: int,
+        body: list = [],
+        cond: NodeBase | None = None,
+        jump_state: int | None = None,
+    ):
+        if state in self.blocks:
+            raise ValueError("Conflict state, check it again")
+
+        if state not in self.states:
+            raise ValueError("Unknown state, check it again")
+
+        self.blocks[state] = BasicBlock(
+            state=state,
+            next_state=next_state,
+            body=body,
+            cond=cond,
+            jump_state=jump_state,
+        )
+
+    def get_bb(self, state):
+        return self.blocks[state]
+
     @staticmethod
     def gen_cfg(body: list):
         cfg = CFG()
@@ -84,55 +109,47 @@ class CFG:
 
         while len(bfs_queue) > 0:
             ss: StateSegment = bfs_queue.popleft()
-            continue_at = getattr(ss, "continue_at", default=None)
-            break_to = getattr(ss, "break_to", default=None)
+            continue_at = getattr(ss, "continue_at", None)
+            break_to = getattr(ss, "break_to", None)
 
             for i in range(len(ss.body)):
                 x = ss.body[i]
 
+                # Jump out statements
                 if x.nodeType == "Continue":
                     if continue_at is None:
                         raise ValueError(
                             "A continue statement in non-loop environment, maybe the AST is broken??"
                         )
-                    new_bb = BasicBlock(
-                        body=ss.body[:i],
-                        state=ss.state,
-                        next_state=continue_at,
-                    )
-                    cfg.blocks[new_bb.state] = new_bb
+                    cfg.add_bb(state=ss.state, next_state=continue_at, body=ss.body[:i])
                     break
                 elif x.nodeType == "Break":
                     if break_to is None:
                         raise ValueError(
                             "A break statement in non-loop environment, maybe the AST is broken??"
                         )
-                    new_bb = BasicBlock(
-                        body=ss.body[:i],
-                        state=ss.state,
-                        next_state=break_to,
-                    )
-                    cfg.blocks[new_bb.state] = new_bb
+                    cfg.add_bb(state=ss.state, next_state=break_to, body=ss.body[:i])
                     break
 
+                # Do BFS on the final branch if present
+                # Otherwise, the final state should be ss's next state
                 if x.nodeType in CFG.BRANCH_STMT:
-                    # bfs on the final branch
                     if i == len(ss.body) - 1:
                         final_state = ss.next_state
                     else:
                         final_state = cfg.gen_state()
                         bfs_queue.append(
                             StateSegment(
-                                body=ss.body[i + 1 :],
                                 state=final_state,
                                 next_state=ss.next_state,
+                                body=ss.body[i + 1 :],
                                 continue_at=continue_at,
                                 break_to=break_to,
                             )
                         )
 
                 if x.nodeType == "IfStatement":
-                    # bfs on the true branch
+                    # Do BFS on the true branch
                     if len(x.trueBody) == 0:
                         true_state = final_state
                     else:
@@ -146,101 +163,109 @@ class CFG:
                                 break_to=break_to,
                             )
                         )
-                    if hasattr(x, "falseBody"):
-                        # bfs on the false branch (if present)
-                        if len(x.falseBody == 0):
-                            false_state = final_state
-                        else:
-                            false_state = cfg.gen_state()
-                            bfs_queue.append(
-                                StateSegment(
-                                    body=x.falseBody,
-                                    state=false_state,
-                                    next_state=final_state,
-                                    continue_at=continue_at,
-                                    break_to=break_to,
-                                )
-                            )
-                        next_state = false_state
-                    else:
-                        next_state = final_state
 
-                    new_bb = BasicBlock(
+                    # Do BFS on the false branch if present
+                    if hasattr(x, "falseBody") and len(x.falseBody) > 0:
+                        false_state = cfg.gen_state()
+                        bfs_queue.append(
+                            StateSegment(
+                                body=x.falseBody,
+                                state=false_state,
+                                next_state=final_state,
+                                continue_at=continue_at,
+                                break_to=break_to,
+                            )
+                        )
+                    else:
+                        false_state = final_state
+
+                    cfg.add_bb(
                         body=ss.body[:i],
                         state=ss.state,
-                        next_state=next_state,
+                        next_state=false_state,
                         cond=x.condition,
                         jump_state=true_state,
                     )
-                    cfg.blocks[new_bb.state] = new_bb
+
                     break
+
                 elif x.nodeType == "ForStatement":
                     cond_state = cfg.gen_state()
+                    loop_state = cfg.gen_state()
+
                     true_state = cfg.gen_state()
                     bfs_queue.append(
                         StateSegment(
-                            body=[*x.nodes, x.loopExpression],
                             state=true_state,
-                            next_state=cond_state,
-                            continue_at=continue_at,
-                            break_to=break_to,
+                            next_state=loop_state,
+                            body=[*x.nodes, x.loopExpression],
+                            continue_at=loop_state,
+                            break_to=final_state,
                         )
                     )
 
-                    cond_bb = BasicBlock(
-                        body=[],
+                    # Add the beginning block
+                    cfg.add_bb(
+                        state=ss.state,
+                        next_state=cond_state,
+                        body=[*ss.body[:i], x.initializationExpression],
+                    )
+
+                    # Add the condition block
+                    cfg.add_bb(
                         state=cond_state,
                         next_state=final_state,
                         cond=x.condition,
                         jump_state=true_state,
                     )
-                    cfg.blocks[cond_bb.state] = cond_bb
 
-                    new_bb = BasicBlock(
-                        body=[*ss.body[:i], x.initializationExpression],
-                        state=ss.state,
-                        next_state=cond_state,
+                    # Add the loop block, eg. i+=1
+                    cfg.add_bb(
+                        state=loop_state, next_state=cond_state, body=[x.loopExpression]
                     )
-                    cfg.blocks[new_bb.state] = new_bb
+
+                    break
+
                 elif x.nodeType.endswith("WhileStatement"):
+                    # Pre-compute state of the condition block
                     cond_state = cfg.gen_state()
+
+                    # Do BFS on while body
                     if len(x.nodes) == 0:
                         true_state = cond_state
                     else:
                         true_state = cfg.gen_state()
                         bfs_queue.append(
                             StateSegment(
-                                body=x.nodes,
                                 state=true_state,
                                 next_state=cond_state,
-                                continue_at=continue_at,
-                                break_to=break_to,
+                                body=x.nodes,
+                                continue_at=cond_state,
+                                break_to=final_state,
                             )
                         )
-                    cond_bb = BasicBlock(
-                        body=[],
+
+                    # Add the beginning block
+                    if x.nodeType == "WhileStatement":  # while
+                        cfg.add_bb(
+                            state=ss.state, next_state=cond_state, body=ss.body[:i]
+                        )
+                    else:  # do-while
+                        cfg.add_bb(
+                            state=ss.state, next_state=true_state, body=ss.body[:i]
+                        )
+
+                    # Add the condition block (*body* is empty)
+                    cfg.add_bb(
                         state=cond_state,
                         next_state=final_state,
                         cond=x.condition,
                         jump_state=true_state,
                     )
-                    cfg.blocks[cond_bb.state] = cond_bb
 
-                    if x.nodeType == "WhileStatement":
-                        next_state = cond_state  # while
-                    else:
-                        next_state = true_state  # do-while
-
-                    new_bb = BasicBlock(
-                        body=ss.body[:i],
-                        state=ss.state,
-                        next_state=next_state,
-                    )
-                    cfg.blocks[new_bb.state] = new_bb
                     break
             else:
-                bb = BasicBlock.of_ss(ss)
-                cfg.blocks[bb.state] = bb
+                cfg.add_bb(ss.state, ss.next_state, body=ss.body)
 
         return cfg
 
@@ -369,6 +394,9 @@ def extractBlockFromTree(node, block_list, level=0):
 
 
 def obfuscate(node):
+
+    cfg = CFG.gen_cfg(body=node[1][5].nodes)
+
     global Index
     # 查询while节点(可能有多个)
     while_Nodes = find_While_and_If_Statement(
